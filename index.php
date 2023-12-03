8335a6341e2011c4f7a4207b6244b547b02dce4e
Taimoor Sardar <tsardar.bese21seecs@seecs.edu.pk>
2023-12-04 02:14:54 +0500

    Updated php file, now extracting info part is complete

--- index.php
+++ index.php
@@ -2,20 +2,41 @@
 $start = "https://google.com";
 
 $alreadycrawled = array();
+$crawling = array();
 
+// this function is used to get details from the urls 
 function  get_details($url){
-    // for giving custome headers
-    $options = array('http'=>array('method'=>"GET", 'headers'=>"User-Agent: crawlBot/0.1 \n"));
+    // for giving custom headers
+    $options = array('http'=>array('method'=>"GET", 'headers'=>"User-Agent: crawlBot/0.1 \n")); // just giving a name to the bot
     $context  = stream_context_create($options); // to mess with headers
     
     $doc = new DOMDocument();
     @$doc->loadHTML('<?xml encoding="UTF-8">'.@file_get_contents($url, false, $context)); //@ sign is to deal with warning
-}
+    
+    $title = $doc->getElementsByTagName("title");
+    $title = $title->item(0)->nodeValue;
 
+    $description = "";
+    $keywords = "";
+    $metas = $doc->getElementsByTagName("meta");
+    for ($i = 0; $i < $metas->length; $i++) {
+		$meta = $metas->item($i);
+		// Get the description and the keywords.
+		if (strtolower($meta->getAttribute("name")) == "description")
+			$description = $meta->getAttribute("content");
+		if (strtolower($meta->getAttribute("name")) == "keywords")
+			$keywords = $meta->getAttribute("content");
+
+    // return in json the relevant information
+	return '{ "Title": "'.str_replace("\n", "", $title).'", "Description": "'.str_replace("\n", "", $description).'", "Keywords": "'.str_replace("\n", "", $keywords).'", "URL": "'.$url.'"},';
+
+    }
+}
 
 function follow_links($url){
 
     global $alreadycrawled;
+    global $crawling;
     // for giving custome headers
     $options = array('http'=>array('method'=>"GET", 'headers'=>"User-Agent: crawlBot/0.1 \n"));
     $context  = stream_context_create($options); // to mess with headers
@@ -62,15 +83,26 @@ function follow_links($url){
         // to check for duplicate links
         if (!in_array ($l, $alreadycrawled)){
             $alreadycrawled[] = $l;
+            $crawling[] = $l;
             echo get_details($l);
         }
+    }
+    array_shift($crawling); // so it doesn't move throught the first item again and again
+    foreach($crawling as $site){
+        follow_links($site);
     }
 }
 
 follow_links($start);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
- 
-include_once("index.php");
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
